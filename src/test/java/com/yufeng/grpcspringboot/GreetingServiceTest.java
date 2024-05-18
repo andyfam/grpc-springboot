1862f595abe2ecb3fab8ab1434c4deb75d862f38
andyfam <andyfam@gmail.com>
2024-05-17 18:30:15 -0600

    initial commit

---
+++ src/test/java/com/yufeng/grpcspringboot/GreetingServiceTest.java
@@ -0,0 +1,43 @@
+package com.yufeng.grpcspringboot;
+
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest(properties = {
+        "grpc.server.inProcessName=test", // Enable inProcess server
+        "grpc.server.port=-1", // Disable external server
+        "grpc.client.inProcess.address=in-process:test" // Configure the client to connect to the inProcess server
+})
+@SpringJUnitConfig(classes = { GreetingServiceIntegrationTestConfiguration.class })
+// Spring doesn't start without a config (might be empty)
+@DirtiesContext // Ensures that the grpc-server is properly shutdown after each test
+// Avoids "port already in use" during tests
+public class GreetingServiceTest {
+    @GrpcClient("inProcess")
+    private GreetServiceGrpc.GreetServiceBlockingStub greetServiceBlockingStub;
+
+    @Test
+    @DirtiesContext
+    void greet_shouldReturnGreeting() {
+        // Arrange
+        String name = "World";
+
+        // Act
+        GreetResponse response = greetServiceBlockingStub.greet(
+                GreetRequest.newBuilder().setName(name).build());
+
+        // Assert
+        assertNotNull(response);
+        assertEquals("Hello, World!", response.getGreeting());
+    }
+}
---
+++ src/test/java/com/yufeng/grpcspringboot/GrpcSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yufeng.grpcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class GrpcSpringbootApplicationTests {
+
+/*	@Test
+	void contextLoads() {
+
+	}*/
+
+}
